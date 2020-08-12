796e7fd9d458312ca86f11908b9409908f50d12b
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-08-12 15:19:40 -0700

    add footer Nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/Navbar';
 import InfoPanel from './Components/InfoPanel'
+import FooterNav from './Components/FooterNav'
 
 function App() {
   return (
     <div>
       <NavBar />
       <InfoPanel />
+      <FooterNav />
     </div>
   );
 }
